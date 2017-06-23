13be478d8e304eb3db3e9be461863b878bbc4b88
george kousouris <georgekousouris@dyn1171-207.wlan.ic.ac.uk>
2017-06-23 15:31:30 +0100

    Spaced added

--- zach.cpp
+++ zach.cpp
@@ -4,16 +4,21 @@
 using namespace std;
 
 int main() {
-  int n = 62;
+  int n = 62; 
+  int k = 0;
   ifstream fin("input");
+  cout<< "new Vector3[] { \n";
   for (int i=0; i<n; i++) {
     float val1, val2;
     fin>> val1>>val2;
     if (val1 ==0 && val2 == 0) 
 	continue;
-    cout<<"new Vector3 (0, "<<val2<<"f, "<<val1<<"f);";
-
-    cout<<endl;
+    k++;
+    cout<<"new Vector3 (0, "<<val2<<"f, "<<val1<<"f), ";
+    if ( k % 3 == 0 ) {
+	cout<<endl;
+    }
   } 
+  cout<<"\n};\n";
   return 0;
 }    
